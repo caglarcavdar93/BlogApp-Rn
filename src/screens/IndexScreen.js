cf4aa5f2009fa03d7154cf2c598962d58eccab6e
Çağlar Çavdar <caglar.cavdar@windowslive.com>
2021-01-11 01:09:23 +0300

    Index screen and blog context added

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,20 @@
+import React,{useContext} from 'react';
+import {View,Text,Button, FlatList, StyleSheet} from 'react-native';
+import BlogContext from '../context/BlogContext';
+const IndexScreen=()=>{
+    const {data,addBlogPost}= useContext(BlogContext);
+    return <View>
+        <Text>Index Screen</Text>
+        <Button title="Add Post" onPress={addBlogPost}/>
+        <FlatList 
+        data={data}
+        keyExtractor={blogPost=>blogPost.title}
+        renderItem={({item})=>{
+            return <Text>{item.title}</Text>
+        }}
+        />
+    </View>
+}
+
+const styles=StyleSheet.create({});
+export default IndexScreen;
